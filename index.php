d110b729dac7d009e74d97f46eabafae7c576655
rk2207 <33948930+rk2207@users.noreply.github.com>
2017-11-28 08:45:00 +0530

    modified
    modified

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 include("config.php");
 include("functions.php");
 $rows = getSponsors($con);
-//echo "<pre>";print_r($rows);
-
 ?>
 <style type="text/css">
 .container{
@@ -21,6 +19,49 @@ $rows = getSponsors($con);
 }
 
 </style>
+
+
+<?php
+if(isset($_REQUEST['submit'])){
+	$agent_code = rand(1111111111,9999999999);
+	$sponsor_code = $_REQUEST['sponsor_code'];
+	$sponsor_data = getSponsorData($con,$sponsor_code);
+	//$max_nos_pair = getMaxPair($con,$sponsor_code); //die;
+	//$empty_flag = checkMaxPairIsEmpty($con,$sponsor_code,$max_nos_pair);
+	$depth_level = $sponsor_data['depth_level'] + 1;
+	
+	$checkChild = checkChildDataExist($con,$sponsor_code);
+	$new_pair_id = $checkChild + 1;
+	if($checkChild%2 != 0){
+		$request_array = array('agent_code' => $agent_code,'sponsor_code'=>$sponsor_code,'pair_id'=> $new_pair_id, 'leg'=> 'Left','depth_level'=>$depth_level);
+
+	} else{
+		
+		$request_array = array('agent_code' => $agent_code,'sponsor_code'=>$sponsor_code,'pair_id'=> $new_pair_id, 'leg'=> 'Right','depth_level'=>$depth_level);
+	}
+	echo "<h1>Sponsor Data</h1>";
+	pr($request_array);
+	echo "<h1>Agent Data</h1>";
+	$agent_data = addAgent($con,$request_array);
+	pr($agent_data);
+}
+
+
+?>
 <div class="container">
-	<a href="/agent-tree/view.php">Tree View</a>
+	<a href="/tree/tree.php">Tree View</a>
+	<form action="<?php echo $_SERVER['PHP_SELF']?>" method="POST">
+		<div class="row">
+			<label>Sponsor</label>
+			<select name="sponsor_code" id="sponsor_code">
+				<option ="">--Select Sponsor--</option>
+				<?php foreach ($rows as $row) {?>
+					<option ="<?php echo $row?>"><?php echo $row?></option>
+				<?php }?>
+			</select>
+		</div>
+		<div class="row">
+			<input type="submit" name="submit" value="Submit"/>
+		</div>
+	</form>
 </div>
